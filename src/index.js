cc294999f523e85b0b3a95c0257830f2168d9d03
march213 <jane.molodetskaya@gmail.com>
2022-04-24 12:18:31 -0400

    zipper animation

--- src/index.js
+++ src/index.js
@@ -9,18 +9,23 @@ const params = {
 
 const two = new Two(params).appendTo(container);
 
-const loopDuration = 60 * 4;
+const loopDuration = 60 * 8; // 8 seconds
+
 const shapes = [];
 const shapesCount = 40;
+const shapeMin = 0;
+const shapeMax = two.width;
+const shapeDiff = shapeMax - shapeMin;
+
 const aDelay = 1 / 120;
 const centerX = two.width / 2;
 const centerY = two.height / 2;
 
 for (let i = 0; i < shapesCount; i++) {
   const shape = two.makeRectangle(centerX, i * 20, 500, 10);
-
   shape.fill = 'rgb(0, 220, 255)';
   shape.noStroke();
+
   shapes.push(shape);
 }
 
@@ -32,8 +37,15 @@ two.bind('update', function (frameCount) {
     const aStart = aDelay * (shapesCount - i);
     const aEnd = aDelay * i;
 
-    const u = utils.mapAndClamp(t, aStart, 1 - aEnd, 0, 1);
-    shape.rotation = utils.easeInOutCubic(u) * Math.PI;
+    let u;
+
+    if (t < 0.5) {
+      u = utils.mapAndClamp(t, aStart, 0.5 - aEnd, 0, 1);
+    } else {
+      u = utils.mapAndClamp(t, 0.5 + aStart, 1 - aEnd, 1, 0);
+    }
+
+    shape.width = shapeMin + shapeDiff * utils.easeInOutCubic(u);
   });
 });
 
